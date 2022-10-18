0eb772b4a4629b7b6493b8458377ea82884d3f42
Louisa Strunk <louisa@MBP-von-Louisa.office.5minds.de>
2022-10-18 12:06:26 +0200

    create board

--- Board.py
+++ Board.py
@@ -1,4 +1,24 @@
 class Board:
     def __init__(self):
-        pass
-    
+        self.board = self.createBoard()
+
+    def createBoard(self) -> list[list[str]]:
+        placeholder = "_"
+
+        emptyBoard = [
+            [placeholder] * 7,
+            [placeholder] * 7,
+            [placeholder] * 7,
+            [placeholder] * 7,
+            [placeholder] * 7,
+            [placeholder] * 7,
+            ["1", "2", "3", "4", "5", "6", "7"]
+        ]
+
+        return emptyBoard
+
+    def showBoard(self) -> None:
+        for line in self.board:
+            for element in line:
+                print("|" + element, end="")
+            print("|")
