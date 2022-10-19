7f804ddd52bbda7096c6e2b4b788cb495591e239
Louisa Strunk <louisa@MBP-von-Louisa.office.5minds.de>
2022-10-19 14:15:22 +0200

    ask for a new game

--- 4Gewinnt.py
+++ 4Gewinnt.py
@@ -1,9 +1,23 @@
 from MainGame import MainGame
 
 def main() -> None:
-    game = MainGame()
-    game.prepare_game()
-    game.play()
+    play = True
+
+    while play:
+        game = MainGame()
+        game.prepare_game()
+        game.play()
+
+        while True:
+            again = input("Would you like to play again? (y/n)")
+            if again == "y":
+                play = True
+                break
+            elif again == "n":
+                play = False
+                break
+            else:
+                print("Enter 'y' for yes or 'n' for no.")
 
 if __name__ == '__main__':
     main()
